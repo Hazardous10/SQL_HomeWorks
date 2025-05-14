766e7daa20d4d851b02969ea52c021c5f9d12b06
Hazardous10 <44596970+Hazardous10@users.noreply.github.com>
2025-05-14 09:25:57 +0500

    Update hw.sql

--- lesson-4/homework/hw.sql
+++ lesson-4/homework/hw.sql
@@ -6,3 +6,15 @@ FROM Products_Discounted
 SELECT DISTINCT CategoryID 
 FROM Categories
 WHERE CategoryID = 3 
+
+SELECT * FROM Products
+WHERE Price > 100
+
+SELECT * FROM SQL_LESSONS.dbo.Customers
+WHERE FirstName LIKE 'A%';
+
+SELECT * FROM SQL_LESSONS.dbo.Products_Discounted
+ORDER BY Price ASC
+
+SELECT * FROM SQL_LESSONS.dbo.Employees
+WHERE Salary >= 60000 AND DepartmentName = 'HR'
