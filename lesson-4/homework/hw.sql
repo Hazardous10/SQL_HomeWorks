31ebd361faf6bb25ebc86fecb3c928ec9456ff29
Hazardous10 <44596970+Hazardous10@users.noreply.github.com>
2025-05-15 17:10:59 +0500

    Update hw.sql

--- lesson-4/homework/hw.sql
+++ lesson-4/homework/hw.sql
@@ -1,3 +1,4 @@
+ Easy-Level Tasks (10)
 SELECT TOP 5 * FROM Employees
 
 SELECT DISTINCT Category
@@ -32,3 +33,33 @@ FROM SQL_LESSONS.dbo.Products_Discounted
 SELECT DISTINCT Category, ProductName 
 FROM SQL_LESSONS.dbo.Products_Discounted
 ORDER BY ProductName DESC
+
+Medium-Level Tasks (10)
+SELECT TOP 10 * FROM Products
+ORDER BY Price DESC
+
+SELECT COALESCE(FirstName, LastName) AS FullName FROM Employees
+
+SELECT DISTINCT Category, Price FROM Products
+
+SELECT * FROM Employees 
+WHERE Age BETWEEN 30 and 40 OR DepartmentName = 'Marketing'
+
+SELECT * FROM Employees
+ORDER BY Salary DESC
+OFFSET 10 rows
+FETCH NEXT 10 ROWS ONLY
+
+
+SELECT * FROM Products
+WHERE Price <= 1000 AND StockQuantity > 50
+ORDER BY StockQuantity ASC
+
+SELECT * FROM Products
+WHERE ProductName LIKE '%e%'
+
+SELECT * FROM Employees
+WHERE DepartmentName IN ('HR', 'IT','Finance')
+
+SELECT * FROM Customers
+ORDER BY City ASC, PostalCode DESC
